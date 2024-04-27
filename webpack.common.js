0973060d44ab94caacffb54e86954fa65e346c6f
IKhlash02 <muhammadikhlash5@gmail.com>
2024-04-27 17:30:32 +0700

    fix bundle analyzer

--- webpack.common.js
+++ webpack.common.js
@@ -105,7 +105,10 @@ module.exports = {
         },
       ],
     }),
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
   ],
 
 };
